d857052593754eb9f0c826b33f4d8426496a36de
Adrián Medina Videra <adrianmedinavidera@gmail.com>
2024-11-20 10:00:18 +0100

    Fabiana te quiero <3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, soy Macarena");
         System.out.println("Hola, soy Fabiana y tengo 5 años");
-        System.out.println("Macarena DICTADORA");
+        System.out.println("Macarena DICTADORA"); //Sí es.
     }
 }
