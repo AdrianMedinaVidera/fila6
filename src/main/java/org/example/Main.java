a5cba89972d10c77466e1090afe22b567ab12b55
_asieergr_ <asierlopezgarcia1@gmail.com>
2024-11-20 09:52:58 +0100

    dictadora

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, soy Macarena");
+        System.out.println("Macarena dictadora");
     }
 }
