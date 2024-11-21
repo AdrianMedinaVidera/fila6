96ea43aafe75182f968d4a70b10a4a0b29de0170
AdrianMedinaVidera <adrianmedinavidera@gmail.com>
2024-11-21 13:26:54 +0100

    TestMismaLinea

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, soy Macarena");
         System.out.println("Hola, soy Fabiana y tengo 26 años");
+        System.out.println("Si");
     }
 }
