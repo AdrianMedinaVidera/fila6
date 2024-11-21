3674dec4981eca195fda765faf7a06f4c14923d6
fabianacb <fabianabarbaticbd@gmail.com>
2024-11-21 13:37:37 +0100

    camp rock

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, soy Macarena");
         System.out.println("Hola, soy Fabiana y tengo 26 años");
-        System.out.println("Fabiana, we love u!");
+        System.out.println("Fabiana, we love u! Camp Rock!");
         System.out.println("Si, no, no");
     }
 }
