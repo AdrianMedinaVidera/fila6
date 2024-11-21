b5b96804ecb408a32d790c5385d642790204e4a6
@maaaaquiiii <80751866+maaaaquiiii@users.noreply.github.com>
2024-11-21 13:14:04 +0100

    fourth commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, soy Macarena");
-        System.out.println("Hola, soy Fabiana y tengo 5 años");
-        System.out.println("Macarena DICTADORA"); //Sí es.
+        System.out.println("Hola, soy Fabiana y tengo 26 años");
     }
 }
