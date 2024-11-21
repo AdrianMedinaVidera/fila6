e546c8a637bcc548449402040bdabf144e1c3c64
fabianacb <fabianabarbaticbd@gmail.com>
2024-11-21 13:28:57 +0100

    segundo commit fabi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,9 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, soy Macarena");
         System.out.println("Hola, soy Fabiana y tengo 26 años");
+        System.out.println("me quiero ir a mi casa por favor");
     }
 }
