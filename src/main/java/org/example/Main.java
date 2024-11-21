93328289b45e0b7800af696672159e93671adf7d
_asieergr_ <asierlopezgarcia1@gmail.com>
2024-11-21 13:26:49 +0100

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola, soy Macarena");
+        System.out.println("Hola, soy Macarena y soy una dictadora");
         System.out.println("Hola, soy Fabiana y tengo 26 años");
     }
 }
